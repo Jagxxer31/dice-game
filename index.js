68892fffec4004f8722c8647be053f1a868462f2
Jagxxer31 <167946914+Jagxxer31@users.noreply.github.com>
2025-03-09 07:55:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var randnuma=Math.floor(Math.random()*6+1);
+var randnumb=Math.floor(Math.random()*6+1);
+var randsrca="./images/dice"+randnuma+".png";
+var randsrcb="./images/dice"+randnumb+".png";
+var im=document.getElementsByClassName(".img1");
+document.querySelector("img.img1").setAttribute("src",randsrca);
+document.querySelector("img.img2").setAttribute("src",randsrcb);
+if(randnuma>randnumb){
+    document.querySelector("h3").textContent="Player 1 has won"
+}else if(randnuma<randnumb){
+    document.querySelector("h3").textContent="Player 2 has won"
+}else if(randnuma==randnumb){
+    document.querySelector("h3").textContent="It's a draw"
+}
