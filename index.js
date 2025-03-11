c1ceeb8b056701c97c6132aef9b7184d6d4c857c
Jagxxer31 <167946914+Jagxxer31@users.noreply.github.com>
2025-03-11 12:29:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var randnuma=Math.floor(Math.random()*6+1);
 var randnumb=Math.floor(Math.random()*6+1);
+
 var randsrca="./images/dice"+randnuma+".png";
 var randsrcb="./images/dice"+randnumb+".png";
+
 var im=document.getElementsByClassName(".img1");
 document.querySelector("img.img1").setAttribute("src",randsrca);
 document.querySelector("img.img2").setAttribute("src",randsrcb);
+
 if(randnuma>randnumb){
     document.querySelector("h3").textContent="Player 1 has won"
 }else if(randnuma<randnumb){
